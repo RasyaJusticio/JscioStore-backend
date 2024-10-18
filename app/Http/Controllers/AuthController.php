aa7f7483a3e074b0660602b18f7b0b3f518efa8e
Rasya Justicio Airlangga <rasyajusticio.public@gmail.com>
2024-10-18 18:15:39 +0700

    feat(requests): add validation for register requests

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\AuthRegisterRequest;
+
+class AuthController extends Controller
+{
+    public function register(AuthRegisterRequest $request) {}
+}
