a0ebe5d3afc650cf7f0c71c2d8869414e647841a
Rasya Justicio Airlangga <rasyajusticio.public@gmail.com>
2024-10-18 18:47:50 +0700

    feat(auth): add register control and endpoint
    - Added register function in `AuthController.php`
    - Updated routes to include the register endpoint

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\AuthRegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
-    public function register(AuthRegisterRequest $request) {}
+    public function register(AuthRegisterRequest $request)
+    {
+        $fields = $request->validated();
+
+        $username = Str::slug($fields['full_name']).'-'.Str::random(8).now()->format('YmdHis');
+
+        User::create([
+            'username' => $username,
+            'full_name' => $fields['full_name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        return $this->success('User registered successfully');
+    }
 }
