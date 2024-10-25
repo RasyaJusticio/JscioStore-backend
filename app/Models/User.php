20e493eaee0ed4211828fbb70bce3c205d78bf08
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-10-25 13:34:18 +0700

    feat: create foreign method to retrieve user's shipping addresses

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,14 +10,8 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'username',
         'full_name',
@@ -27,26 +21,21 @@ class User extends Authenticatable
         'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function shipAddresses()
+    {
+        return $this->hasMany(ShipAddress::class);
     }
 }
