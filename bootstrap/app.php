dc19c1db276e95693f68f9e0a8cec87dfe22eef2
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-11-04 13:50:37 +0700

    feat: implement exception handlers for invalid routes, not found resources, and auth failures

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Middleware\AdminOnlyMiddleware;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -30,5 +32,30 @@ return Application::configure(basePath: dirname(__DIR__))
                     'message' => 'CSRF token mismatch',
                 ], 419);
             }
+        });
+
+        $exceptions->render(function (NotFoundHttpException $exception) {
+            $exception_msg = $exception->getMessage();
+
+            if (str_contains($exception_msg, 'No query results')) {
+                return response()->json([
+                    'status' => 'fail',
+                    'message' => 'Resource not found'
+                ], 404);
+            }
+
+            if (str_contains($exception_msg, 'The route')) {
+                return response()->json([
+                    'status' => 'fail',
+                    'message' => 'Route not found'
+                ], 404);
+            }
+        });
+
+        $exceptions->render(function (AuthenticationException $exception) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Unauthenticated'
+            ], 401);
         });
     })->create();
