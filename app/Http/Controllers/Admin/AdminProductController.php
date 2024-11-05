4fad97632e12ea78ad41f059ee128f4111980150
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-11-05 10:39:08 +0700

    feat(product): add product show endpoint

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -34,7 +34,7 @@ class AdminProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return $this->success('Product fetched successfully', $product);
     }
 
     /**
