7462295f6139597109df67377bd85ed7ca6627c2
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-11-11 10:39:51 +0700

    fix(product-image): correct fillable attribute typo in product image model

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    protected $fillalble = [
+    protected $fillable = [
         'product_id',
         'url',
     ];
