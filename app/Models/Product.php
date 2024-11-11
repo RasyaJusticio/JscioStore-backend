9535fd72d2da468bcb36e36a5a96426569ed32d1
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-11-11 10:11:57 +0700

    feat: establish product relationship with product images

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -17,4 +17,9 @@ class Product extends Model
     {
         return $this->belongsToMany(Category::class, 'product_categories', 'product_id', 'category_id');
     }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
 }
